80fae1bcbd67b16302001e6e59554696f7d42620
Thanatisia <55834101+Thanatisia@users.noreply.github.com>
2022-04-25 18:14:33 +0800

    Modified indentation

--- src/lib/toolset/cliargs.py
+++ src/lib/toolset/cliargs.py
@@ -26,28 +26,28 @@ class Initialize():
 				Optional: True
 				Default: None
 
-            parser_opts : Options to pass to the generating parser
-                Type: Dictionary (Kwargs)
-                Optional: True
-                Default: None
-                Values:
-                    Key : Parameter Name
-                    Value : Parameter value
-
+            		parser_opts : Options to pass to the generating parser
+                		Type: Dictionary (Kwargs)
+                		Optional: True
+                		Default: None
+                		Values:
+                		    Key : Parameter Name
+                		    Value : Parameter value
+	
 			arguments : All Arguments and their definitions here
 				Type: List
 				Values:
 					[
 						# Argument 1
 						{
-                            "flags" : ["flag-or-string"],
+                					"flags" : ["flag-or-string"],
 							"optionals" : {
 								# ...
 							}
 						},
 						# Argument 2
 						{
-                            "flags" : ["flag-or-string"],
+                           		 		"flags" : ["flag-or-string"],
 							"optionals" : {
 								# ...
 							}
@@ -57,15 +57,15 @@ class Initialize():
 				Optional: True
 				Default: None
 
-            arg_list : A List of strings to parse for parse_args()
-                Type: List
-                Optional: True
-                Default: None
+			arg_list : A List of strings to parse for parse_args()
+				Type: List
+                		Optional: True
+                		Default: None
 
-            namespace : A Namespace object to hold the variables
-                Type: Namespace
-                Optional: True
-                Default: None
+            		namespace : A Namespace object to hold the variables
+                		Type: Namespace
+                		Optional: True
+                		Default: None
 		"""
 
 		# Initialize Command Line Interface (CLI) Argument Controller class
@@ -77,15 +77,15 @@ class Initialize():
 		for i in range(number_of_arguments):
 			curr_arg = arguments[i]
 			
-            # Retrieve Argument Details
-            # List
-            curr_arg_name_or_flags = curr_arg["flags"]
+            		# Retrieve Argument Details
+            		# List
+            		curr_arg_name_or_flags = curr_arg["flags"]
 
-            if "optionals" in curr_arg:
-                # Dictionary
-                curr_arg_optionals = curr_arg["optionals"]
-            else:
-                curr_arg_optionals = {}
+            		if "optionals" in curr_arg:
+                		# Dictionary
+                		curr_arg_optionals = curr_arg["optionals"]
+            		else:
+               		 	curr_arg_optionals = {}
 
 			# Add current argument to Argument Parser
 			self.cliargs.arg_add(curr_arg_name_or_flags, self.parser, **curr_arg_optionals)
